cf152dfdcd7f1ab86f05cd738a2f47fa7ca9338c
Jaime Marro <jaime28marro@gmail.com>
2026-06-19 23:32:14 -0600

    Migraciones automaticas en produccion

--- GradoCeroV1/Program.cs
+++ GradoCeroV1/Program.cs
@@ -41,4 +41,10 @@ app.UseAntiforgery();
 app.MapRazorComponents<App>()
    .AddInteractiveServerRenderMode();
 
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.Migrate();
+}
+
 app.Run();
