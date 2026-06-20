d9fa3c9404d2d720aa0eec13d27cf09091bfff92
Jaime Marro <jaime28marro@gmail.com>
2026-06-19 23:49:57 -0600

    Agregar migracion PostgreSQL

--- GradoCeroV1/Migrations/20260620054905_PostgreSQLMigration.Designer.cs
+++ GradoCeroV1/Migrations/20260620054905_PostgreSQLMigration.Designer.cs
@@ -3,6 +3,7 @@ using GradoCeroV1.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace GradoCeroV1.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260620054905_PostgreSQLMigration")]
+    partial class PostgreSQLMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
