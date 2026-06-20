826647034c1afd4752a2e839b108a3fec6da2b5c
Jaime Marro <jaime28marro@gmail.com>
2026-06-19 22:35:09 -0600

    Preparar para deploy en Railway con PostgreSQL

--- GradoCeroV1/Program.cs
+++ GradoCeroV1/Program.cs
@@ -2,15 +2,25 @@ using GradoCeroV1.Data;
 using GradoCeroV1.Components;
 using GradoCeroV1.Services;
 using Microsoft.EntityFrameworkCore;
+using Npgsql.EntityFrameworkCore.PostgreSQL;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
-builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 
+if (builder.Environment.IsProduction())
+{
+    builder.Services.AddDbContext<AppDbContext>(options =>
+        options.UseNpgsql(connectionString));
+}
+else
+{
+    builder.Services.AddDbContext<AppDbContext>(options =>
+        options.UseSqlServer(connectionString));
+}
 builder.Services.AddScoped<TransposicionService>();
 builder.Services.AddScoped<EstadoSesion>();
 
